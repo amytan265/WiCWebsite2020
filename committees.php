8cfe85955e46abea1cf94071d97b4bd0de67dee3
amy <amytan265@gmail.com>
2021-03-31 04:10:04 -0400

    bulk update

--- committees.php
+++ committees.php
@@ -4,13 +4,12 @@
     <meta charset="utf-8" />
     <title>Home | Women in Computing</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
     <link href="style.css" rel="stylesheet" type="text/css">
     <script src="js/navi.js"></script>
 </head>
 <body>
-
-</body>
 
 <?php include 'nav.html';?>
 
@@ -184,5 +183,7 @@
     </div>
 <!-- end of page container -->
 </div>
+
+</body>
 
 </html>
